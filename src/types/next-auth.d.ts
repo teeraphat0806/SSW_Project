78ae06ed5d62aebc535814c4507d5d53b4ad44a2
Teeraphat Jirapasnitilet <110732968+teeraphat0806@users.noreply.github.com>
2025-11-07 16:10:43 +0700

    new chatbot-api

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,29 +1,32 @@
-
+import "next-auth";
 
 declare module "next-auth" {
   interface Session {
     user: {
-      id: string
-      name: string
-      email: string
-      role: string
-      image?: string
-    }
+      id: string;
+      name: string;
+      email: string;
+      role: string;
+      image?: string;
+    };
   }
 
   interface User {
-    id: string
-    name: string
-    email: string
-    role: string
+    id: string;
+    name: string;
+    email: string;
+    role: string;
   }
+}
+
+declare module "next-auth" {
+  export * from "next-auth/core/types";
+  export { getServerSession } from "next-auth/next";
 }
 
 declare module "next-auth/jwt" {
   interface JWT {
-    id: string
-    role: string
+    id: string;
+    role: string;
   }
 }
-
-
